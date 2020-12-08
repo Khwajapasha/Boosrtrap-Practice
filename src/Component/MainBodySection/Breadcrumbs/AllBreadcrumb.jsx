642b836dc9d5ab1af5b725f82ac2d7cb068ba91e
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-08 10:57:26 +0530

    Implemented onClick event to BreadCrumbs

--- src/Component/MainBodySection/Breadcrumbs/AllBreadcrumb.jsx
+++ src/Component/MainBodySection/Breadcrumbs/AllBreadcrumb.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import { BreadCrumbsComponent } from "./BreadCrumbsComponent";
-
-export const AllBreadcrumb = (props) => {
+import { Button } from "react-bootstrap";
+export const AllBreadcrumb = () => {
+  const [show,setShow]=useState(false)
   return (
-    <div>
-      <BreadCrumbsComponent />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <BreadCrumbsComponent />
+        </div>
+      )}
+      <div>
+        <Button onClick={() => setShow(!show)}>Click To Load Breadcrumb</Button>
+      </div>
+    </React.Fragment>
   );
 };
