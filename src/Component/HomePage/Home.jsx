666d463ac76e63f885d17e0e11ed321059a8a62a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-10 14:46:59 +0530

    Implemented Chatbot

--- src/Component/HomePage/Home.jsx
+++ src/Component/HomePage/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { ChatBoxLayout} from "../ChatBox/ChatBoxLayout";
 import { MainBodySection } from "../MainBodySection/MainBodySection";
 export const Home = () => {
   return (
     <div>
       <MainBodySection />
+      
+      <ChatBoxLayout/>
     </div>
   );
 };
