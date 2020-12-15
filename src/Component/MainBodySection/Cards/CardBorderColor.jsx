6f7aad6e6360931baf5f0aa591dc724c18893777
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-15 12:27:36 +0530

    Implemented card border color success

--- src/Component/MainBodySection/Cards/CardBorderColor.jsx
+++ src/Component/MainBodySection/Cards/CardBorderColor.jsx
@@ -1,15 +1,37 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 export const CardBorderColor = () => {
   return (
-    <div>
-      <Card border="primary" style={{ width: "18rem" }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Primary Card Title</Card.Title>
-          <Card.Text>Primary Card Border </Card.Text>
-        </Card.Body>
-      </Card>
+    <div className="mt-2">
+      <Row>
+        <Card border="primary" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Primary Card Title</Card.Title>
+            <Card.Text>Primary Card Border </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card border="secondary" style={{ width: "18rem", left: "2px" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Secondary Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card border="success" style={{ width: "18rem", left: "2px" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Success Border</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Row>
     </div>
   );
 };
