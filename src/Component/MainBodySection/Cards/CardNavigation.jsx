3e89a875267c83ea18cf61a248da8efe9f98bfec
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-15 10:03:24 +0530

    Implemented Navigation Pills

--- src/Component/MainBodySection/Cards/CardNavigation.jsx
+++ src/Component/MainBodySection/Cards/CardNavigation.jsx
@@ -20,9 +20,33 @@ export const CardNavigation = () => {
             </Nav>
           </Card.Header>
           <Card.Body>
-            <Card.Title>Navigation In Card</Card.Title>
+            <Card.Title>Links In Card</Card.Title>
             <Card.Text>
-             This is the example of navigation links in card... 
+              This is the example of navigation links in card...
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card className="mt-1">
+          <Card.Header>
+            <Nav variant="pills" defaultActiveKey="#first">
+              <Nav.Item>
+                <Nav.Link href="#first">Active</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#disabled" disabled>
+                  Disabled
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>Pills In Card</Card.Title>
+            <Card.Text>
+              This is the example of navigation Pills in card...
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
