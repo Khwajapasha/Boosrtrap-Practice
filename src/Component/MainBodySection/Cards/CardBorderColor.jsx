e9e0412a9f9558b9b8749a37057f9e6db2a0ae96
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-15 12:30:15 +0530

    Implemented all border color of card

--- src/Component/MainBodySection/Cards/CardBorderColor.jsx
+++ src/Component/MainBodySection/Cards/CardBorderColor.jsx
@@ -31,6 +31,65 @@ export const CardBorderColor = () => {
             </Card.Text>
           </Card.Body>
         </Card>
+        <Card border="danger" style={{ width: "18rem", left: "2px" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Danger Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="warning" style={{ width: "18rem", left: "2px" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Warning Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="info" style={{ width: "18rem", left: "2px" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Info Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="dark" style={{ width: "18rem", left: "2px" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Dark Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="light" style={{ width: "18rem", left: "2px" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Light Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
       </Row>
     </div>
   );
