ad9e18ffef8fae96a97b2c9ff3d41a968d47f194
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-16 12:19:54 +0530

    Implemented Carousel basic with perfect images

--- src/Component/MainBodySection/CarouselsSection/CarouselBasicExample.jsx
+++ src/Component/MainBodySection/CarouselsSection/CarouselBasicExample.jsx
@@ -1,48 +1,50 @@
-import React from 'react';
-import{Carousel} from "react-bootstrap"
+import React from "react";
+import { Carousel } from "react-bootstrap";
 export const CarouselBasicExample = () => {
-    return (
-      <div>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Third slide"
-            />
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="Assets/Sample1.jpg"
+            alt="First slide"
+            style={{ width: "640px", height: "400px" }}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 "
+            src="Assets/Sample2.jpg"
+            alt="Third slide"
+            style={{ width: "640px", height: "400px" }}
+          />
 
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="Assets/Sample3.jpg"
+            alt="Third slide"
+            style={{ width: "640px", height: "400px" }}
+          />
 
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
-    );
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
-
