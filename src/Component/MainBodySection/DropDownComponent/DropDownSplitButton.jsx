0686fa47d58aed651e4aad8a90a00a6b7d6f13ce
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-22 11:01:34 +0530

    DropDown color dynamically loaded & SplitButton used

--- src/Component/MainBodySection/DropDownComponent/DropDownSplitButton.jsx
+++ src/Component/MainBodySection/DropDownComponent/DropDownSplitButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Dropdown, ButtonGroup } from "react-bootstrap";
+import { Button, Dropdown, ButtonGroup, SplitButton } from "react-bootstrap";
 export const DropDownSplitButton = () => {
   return (
     <div className="mt-2">
@@ -17,6 +17,28 @@ export const DropDownSplitButton = () => {
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
+      <br />
+      === Color Dynamically loaded & used SplitButton ===
+      <br />
+      {["Primary", "Secondary", "Success", "Info", "Warning", "Danger"].map(
+        (variant) => (
+          <SplitButton
+            key={variant}
+            id={`dropdown-split-variants-${variant}`}
+            variant={variant.toLowerCase()}
+            title={variant}
+            className="ml-2"
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3" active>
+              Active Item
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </SplitButton>
+        )
+      )}
     </div>
   );
 };
