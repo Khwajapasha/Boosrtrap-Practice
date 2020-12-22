838738576537c78a6533e00ecceeb23cfe62c360
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-22 11:19:34 +0530

    Dropdown types (Button & Split)

--- src/Component/MainBodySection/DropDownComponent/AllDropDown.jsx
+++ src/Component/MainBodySection/DropDownComponent/AllDropDown.jsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import {DropDownBasicExample} from './DropDownBasicExample';
-import {Button} from "react-bootstrap"
-import { DropDownDynamicColors } from './DropDownDynamicColors';
-import {DropDownSplitButton} from './DropDownSplitButton';
+import React, { useState } from "react";
+import { DropDownBasicExample } from "./DropDownBasicExample";
+import { Button } from "react-bootstrap";
+import { DropDownDynamicColors } from "./DropDownDynamicColors";
+import { DropDownSplitButton } from "./DropDownSplitButton";
+import { DropDownSizing } from "./DropDownSizing";
 export const AllDropDown = () => {
-    const [show,setShow]=useState(false)
-    return (
-      <React.Fragment>
-        {show && (
-          <div>
-            <DropDownBasicExample />
-            <DropDownDynamicColors/>
-            <DropDownSplitButton/>
-          </div>
-        )}
-        <div className="mt-2">
-          <Button onClick={() => setShow(!show)}>Load all DropDowns</Button>
+  const [show, setShow] = useState(false);
+  return (
+    <React.Fragment>
+      {show && (
+        <div>
+          <DropDownBasicExample />
+          <DropDownDynamicColors />
+          <DropDownSplitButton />
+          <DropDownSizing />
         </div>
-      </React.Fragment>
-    );
+      )}
+      <div className="mt-2">
+        <Button onClick={() => setShow(!show)}>Load all DropDowns</Button>
+      </div>
+    </React.Fragment>
+  );
 };
-
---
+++ src/Component/MainBodySection/DropDownComponent/DropDownSizing.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  DropdownButton,
+  SplitButton,
+  Dropdown,
+  ButtonGroup,
+} from "react-bootstrap";
+export const DropDownSizing = () => {
+  return (
+    <div>
+      <br />
+      ===DropDown Types (Button & Split ) ===
+      <br />
+      <div className="mb-2 ml-2">
+        {[DropdownButton, SplitButton].map((DropdownType, idx) => (
+          <DropdownType
+            as={ButtonGroup}
+            key={idx}
+            id={`dropdown-button-drop-${idx}`}
+            size="lg"
+            title="Drop large"
+            className="mt-2 ml-2"
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownType>
+        ))}
+      </div>
+      <div>
+        {[DropdownButton, SplitButton].map((DropdownType, idx) => (
+          <DropdownType
+            as={ButtonGroup}
+            key={idx}
+            id={`dropdown-button-drop-${idx}`}
+            size="md"
+            variant="secondary"
+            title="Drop small"
+            className="mt-2 ml-2"
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownType>
+        ))}
+      </div>
+      <div>
+        {[DropdownButton, SplitButton].map((DropdownType, idx) => (
+          <DropdownType
+            as={ButtonGroup}
+            key={idx}
+            id={`dropdown-button-drop-${idx}`}
+            size="sm"
+            variant="secondary"
+            title="Drop small"
+            className="mt-2 ml-2"
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownType>
+        ))}
+      </div>
+    </div>
+  );
+};
