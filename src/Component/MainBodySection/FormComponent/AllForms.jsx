2e14c8ffdce7e00b4cadf96e4f27b3e4a1f75e92
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-23 14:40:58 +0530

    click to load all form

--- src/Component/MainBodySection/FormComponent/AllForms.jsx
+++ src/Component/MainBodySection/FormComponent/AllForms.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { FormControlComp } from "./FormControlComp";
 import { FormBasic } from "./FormBasic";
 import { FormForChooseFile } from "./FormForChooseFile";
 
 export const AllForms = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <FormBasic />
-      <FormControlComp />
-      <FormForChooseFile />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <FormBasic />
+          <FormControlComp />
+          <FormForChooseFile />
+        </div>
+      )}
+      <div className="mt-2">
+        <Button onClick={() => setShow(!show)}>Load All Form</Button>
+      </div>
+    </React.Fragment>
   );
 };
