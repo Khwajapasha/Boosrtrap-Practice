cc9bf66ee588f12a95da9620bc503264a9c7ae73
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-23 14:26:16 +0530

    Form Control as Select

--- src/Component/MainBodySection/FormComponent/FormControlComp.jsx
+++ src/Component/MainBodySection/FormComponent/FormControlComp.jsx
@@ -7,10 +7,20 @@ export const FormControlComp = () => {
       === Form Control ===
       <br />
       <br />
-      <Form>
+      <Form style={{ width: "18rem", marginLeft: "30rem" }}>
         <Form.Group controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Form Control As select</Form.Label>
+          <Form.Control as="select">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
         </Form.Group>
       </Form>
     </div>
