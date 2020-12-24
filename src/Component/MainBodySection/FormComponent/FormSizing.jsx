48f6f710fadafdd16e152de652eb1dc109af2149
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-24 10:07:40 +0530

    Medium size form

--- src/Component/MainBodySection/FormComponent/FormSizing.jsx
+++ src/Component/MainBodySection/FormComponent/FormSizing.jsx
@@ -3,8 +3,14 @@ import { Form } from "react-bootstrap";
 export const FormSizing = () => {
   return (
     <div>
+      <br />
       === Large Form ===
+      <br />
       <Form.Control size="lg" type="text" placeholder="Large text" />
+      <br />
+      === Medium Form ===
+      <Form.Control size="md" type="text" placeholder="Large text" />
+      <br />
     </div>
   );
 };
