85ef059dc37e4d502adf109391f686d78647cd55
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-25 11:29:46 +0530

    CheckBox & Radio button in horizental line

--- src/Component/MainBodySection/FormComponent/CheckBoxAndRedioButton.jsx
+++ src/Component/MainBodySection/FormComponent/CheckBoxAndRedioButton.jsx
@@ -27,6 +27,18 @@ export const CheckBoxAndRedioButton = () => {
           </div>
         ))}
       </Form>
+      === CheckBox & Radio button in horizental line ===
+      <br />
+      <Form>
+        {["checkbox", "radio"].map((type) => (
+          <div key={`inline-${type}`} className="mb-3">
+            <Form.Check inline label="1" type={type} id={`inline-${type}-1`} />
+            <Form.Check inline label="2" type={type} id={`inline-${type}-2`} />
+            <Form.Check inline label="3" type={type} id={`inline-${type}-3`} />
+            <Form.Check inline label="4" type={type} id={`inline-${type}-4`} />
+          </div>
+        ))}
+      </Form>
     </div>
   );
 };
