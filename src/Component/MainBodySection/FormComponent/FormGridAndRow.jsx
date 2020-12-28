0afbf45608cb6ff52b037d817afdac8f15aa1e51
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-28 10:44:38 +0530

    Form grid using Form.Row

--- src/Component/MainBodySection/FormComponent/FormGridAndRow.jsx
+++ src/Component/MainBodySection/FormComponent/FormGridAndRow.jsx
@@ -4,7 +4,7 @@ export const FormGridAndRow = () => {
   return (
     <div>
       <br />
-      === Form Grid (Row & Col) ===
+      === Form Grid (Using Row & Col) ===
       <Form>
         <Row>
           <Col>
@@ -20,35 +20,22 @@ export const FormGridAndRow = () => {
             <Form.Control placeholder="Last name" />
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <Form.Control placeholder="First name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Last name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="First name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Last name" />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Control placeholder="First name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Last name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="First name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Last name" />
-          </Col>
-        </Row>
       </Form>
+      <div>
+        <br />
+        === Form Grid (Using Form.Row) ===
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Form.Row>
+      </div>
     </div>
   );
 };
