e62adac86e81f72725618128fc1d875a5c98d69d
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-29 12:40:36 +0530

    multiple Add On implemeted

--- src/Component/MainBodySection/InputGroupComponent/MultipleInputGroupsAddOn.jsx
+++ src/Component/MainBodySection/InputGroupComponent/MultipleInputGroupsAddOn.jsx
@@ -3,13 +3,37 @@ import { InputGroup, FormControl } from "react-bootstrap";
 export const MultipleInputGroupsAddOn = () => {
   return (
     <div>
-      <InputGroup className="mb-3">
-        <InputGroup.Prepend>
-          <InputGroup.Text>First and last name</InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl />
-        <FormControl />
-      </InputGroup>
+      <div>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text>First and last name</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl />
+          <FormControl />
+        </InputGroup>
+      </div>
+      <div>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text>$</InputGroup.Text>
+            <InputGroup.Text>0.00</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="Recipient's username"
+            aria-label="Amount (to the nearest dollar)"
+          />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Recipient's username"
+            aria-label="Amount (to the nearest dollar)"
+          />
+          <InputGroup.Append>
+            <InputGroup.Text>$</InputGroup.Text>
+            <InputGroup.Text>0.00</InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+      </div>
     </div>
   );
 };
