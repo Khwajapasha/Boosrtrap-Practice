ed9bf7b519b4c6f62957011d561daf0e2c8ee844
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-29 12:30:01 +0530

    onClick implemented on all input groups

--- src/Component/MainBodySection/InputGroupComponent/AllInputGroups.jsx
+++ src/Component/MainBodySection/InputGroupComponent/AllInputGroups.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { InputGroupBasicExample } from "./InputGroupBasicExample";
 export const AllInputGroups = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <InputGroupBasicExample />
-    </div>
+    <React.Fragment>
+      <div>
+        <InputGroupBasicExample />
+      </div>
+      <div>
+        <Button onClick={() => setShow(!show)}>Load all input groups</Button>
+      </div>
+    </React.Fragment>
   );
 };
