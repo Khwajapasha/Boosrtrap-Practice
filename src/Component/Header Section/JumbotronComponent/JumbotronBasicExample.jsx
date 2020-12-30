c447d4faa183a0735f977497079163dd56d76fae
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-30 15:59:00 +0530

    Slider tag removed

--- src/Component/Header Section/JumbotronComponent/JumbotronBasicExample.jsx
+++ src/Component/Header Section/JumbotronComponent/JumbotronBasicExample.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Jumbotron, Button, Image } from "react-bootstrap";
-import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
 export const JumbotronBasicExample = () => {
   return (
     <div>
       <Jumbotron>
         <h1>Basic Example Of Jumbotron</h1>
-        <Slider>
+        <marquee>
           <Image src="Assets/NewPickUpBizLogo.png" roundedCircle />
-        </Slider>
+        </marquee>
         <p>
           This is the basic example of jumbotron by using Jumbotron tag and it
           is having one button
