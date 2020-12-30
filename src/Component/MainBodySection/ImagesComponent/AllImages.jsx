60ce0ae345c2098eed41e1d941e13f8025f37e8f
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-30 15:32:47 +0530

    Implemented onClcik on all images component

--- src/Component/MainBodySection/ImagesComponent/AllImages.jsx
+++ src/Component/MainBodySection/ImagesComponent/AllImages.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { FigureImage } from "./FigureImage";
 import { ImagesBasicShapes } from "./ImagesBasicShapes";
 import { ImagesFluid } from "./ImagesFluid";
 export const AllImages = () => {
+  const [show, setShow] = useState(false);
   return (
     <React.Fragment>
-      <div>
-        <ImagesBasicShapes />
-        <ImagesFluid />
-        <FigureImage />
+      {show && (
+        <div>
+          <ImagesBasicShapes />
+          <ImagesFluid />
+          <FigureImage />
+        </div>
+      )}
+      <div className="mt-2">
+        <Button onClick={() => setShow(!show)}>Load All Images</Button>
       </div>
     </React.Fragment>
   );
