c597f01d9e547948c6d947ee89ce61f9f24bb4c1
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-30 15:29:07 +0530

    Implemented onCllick on jumbotron

--- src/Component/Header Section/JumbotronComponent/AllJumbotron.jsx
+++ src/Component/Header Section/JumbotronComponent/AllJumbotron.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { JumbotronBasicExample } from "./JumbotronBasicExample";
+import { JumbotronFluid } from "./JumbotronFluid";
 export const AllJumbotron = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <JumbotronBasicExample />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <JumbotronBasicExample />
+          <JumbotronFluid />
+        </div>
+      )}
+      <div className="mb-2 mt-2">
+        <Button onClick={() => setShow(!show)}>Load All Jumbotron</Button>
+      </div>
+    </React.Fragment>
   );
 };
---
+++ src/Component/Header Section/JumbotronComponent/JumbotronFluid.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Jumbotron } from "react-bootstrap";
+export const JumbotronFluid = () => {
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal
+            space of its parent.
+          </p>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
