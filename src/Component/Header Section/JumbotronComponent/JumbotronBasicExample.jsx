a4eec4b261ae13537a41fad43987c6d998f1ee56
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-30 15:18:28 +0530

    implemented visual effect using marquee tag

--- src/Component/Header Section/JumbotronComponent/JumbotronBasicExample.jsx
+++ src/Component/Header Section/JumbotronComponent/JumbotronBasicExample.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Jumbotron, Button } from "react-bootstrap";
+import { Jumbotron, Button, Image } from "react-bootstrap";
 export const JumbotronBasicExample = () => {
   return (
     <div>
       <Jumbotron>
         <h1>Basic Example Of Jumbotron</h1>
+        <marquee>
+          <Image src="Assets/NewPickUpBizLogo.png" roundedCircle />
+        </marquee>
         <p>
           This is the basic example of jumbotron by using Jumbotron tag and it
           is having one button
