b94ebb1ae0aa3debf2f2910b19081f66a87ccaad
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2020-12-31 14:46:20 +0530

    Static backdrop Modal

--- src/Component/MainBodySection/ModalComponent/AllModal.jsx
+++ src/Component/MainBodySection/ModalComponent/AllModal.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ModalBasicExample } from "./ModalBasicExample";
 import { ModalFunctionalClickable } from "./ModalFunctionalClickable";
+import { ModalStaticBackdrop } from "./ModalStaticBackdrop";
 
 export const AllModal = () => {
   return (
     <div>
       <ModalBasicExample />
       <ModalFunctionalClickable />
+      <ModalStaticBackdrop />
     </div>
   );
 };
---
+++ src/Component/MainBodySection/ModalComponent/ModalStaticBackdrop.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+export const ModalStaticBackdrop = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div className="mt-2">
+      <Button variant="primary" onClick={handleShow}>
+        Launch static backdrop modal
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
