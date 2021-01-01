e941ca4b29a38558955c44cec2c22f042dab9a9a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-01 11:20:48 +0530

    Modal vertically centered

---
+++ src/Component/MainBodySection/ModalComponent/ModalVerticallCenter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModalVerticallCenter = () => {
+  return <div></div>;
+};
+
+export default ModalVerticallCenter;
--- src/Component/MainBodySection/ModalComponent/ModalWithoutAnimation.jsx
+++ src/Component/MainBodySection/ModalComponent/ModalWithoutAnimation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
 export const ModalWithoutAnimation = () => {
