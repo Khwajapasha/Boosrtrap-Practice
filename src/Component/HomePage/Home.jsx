a70c0df7dfdc61d50853887761bca44b342a717b
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-04 14:23:28 +0530

    Implemented Navigation bar

--- src/Component/HomePage/Home.jsx
+++ src/Component/HomePage/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { HeaderSection } from "../Header Section/HeaderSection";
 import { MainBodySection } from "../MainBodySection/MainBodySection";
 import { LeftNavigationSection } from "../LeftNavigationSection/LeftNavigationSection";
+import NavigationBar from "./NavigationBar";
 export const Home = () => {
   return (
     <div>
+      <NavigationBar />
       <HeaderSection />
       <LeftNavigationSection />
       <MainBodySection />
