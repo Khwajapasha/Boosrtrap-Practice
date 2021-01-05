9e1f61ea48635ab02114bba4a160921be8d24a6e
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-05 11:03:50 +0530

    NavAlignment

---
+++ src/Component/MainBodySection/NavsComponent/NavAlignment.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const NavAlignment = () => {
+  return (
+    <div>
+      <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
+  );
+};
