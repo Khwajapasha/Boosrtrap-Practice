7c340b75e897e1b922e26bb8dc53d63063c7b602
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-05 12:19:07 +0530

    implement onClick on all Nav component

--- src/Component/MainBodySection/NavsComponent/AllNavs.jsx
+++ src/Component/MainBodySection/NavsComponent/AllNavs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavAlignment } from "./NavAlignment";
 import { NavBasicExample } from "./NavBasicExample";
 import { NavsAsLi } from "./NavsAsLi";
@@ -7,18 +7,26 @@ import { NavAsTab } from "./NavAsTab";
 import { NavAsPills } from "./NavAsPills";
 import { NavFillAndJustifyContentVariable } from "./NavFillAndJustifyContentVariable";
 import { NavSameWidthItem } from "./NavSameWidthItem";
-
+import { Button } from "react-bootstrap";
 export const AllNavs = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <NavBasicExample />
-      <NavsAsLi />
-      <NavAlignment />
-      <NavVerticalAlignment />
-      <NavAsTab />
-      <NavAsPills />
-      <NavFillAndJustifyContentVariable />
-      <NavSameWidthItem />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <NavBasicExample />
+          <NavsAsLi />
+          <NavAlignment />
+          <NavVerticalAlignment />
+          <NavAsTab />
+          <NavAsPills />
+          <NavFillAndJustifyContentVariable />
+          <NavSameWidthItem />
+        </div>
+      )}
+      <div className="mt-2">
+        <Button onClick={() => setShow(!show)}>Load All Navs</Button>
+      </div>
+    </React.Fragment>
   );
 };
