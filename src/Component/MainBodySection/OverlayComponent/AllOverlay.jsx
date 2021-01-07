4408ba86f42cbd105a8768163818a978fe8493df
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-07 13:38:56 +0530

    Implemented onClick on All Overlay

--- src/Component/MainBodySection/OverlayComponent/AllOverlay.jsx
+++ src/Component/MainBodySection/OverlayComponent/AllOverlay.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { OverlayBasicExample } from "./OverlayBasicExample";
 import { OverlayTriggerComp } from "./OverlayTriggerComp";
 import { OverlayCustomizingOverlayBehavior } from "./OverlayCustomizingOverlayBehavior";
 
 export const AllOverlay = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <OverlayBasicExample />
-      <OverlayTriggerComp />
-      <OverlayCustomizingOverlayBehavior />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <OverlayBasicExample />
+          <OverlayTriggerComp />
+          <OverlayCustomizingOverlayBehavior />
+        </div>
+      )}
+      <div className="mt-2">
+        <Button onClick={() => setShow(!show)}>Load All Overlay</Button>
+      </div>
+    </React.Fragment>
   );
 };
