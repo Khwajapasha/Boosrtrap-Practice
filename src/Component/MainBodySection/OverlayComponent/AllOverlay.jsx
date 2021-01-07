99ef223b56566b6b16812dba33718e5db24f36e6
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-07 13:14:30 +0530

    Implemented OverlayTrigger

--- src/Component/MainBodySection/OverlayComponent/AllOverlay.jsx
+++ src/Component/MainBodySection/OverlayComponent/AllOverlay.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { OverlayBasicExample } from "./OverlayBasicExample";
+import { OverlayTriggerComp } from "./OverlayTriggerComp";
 
 export const AllOverlay = () => {
   return (
     <div>
       <OverlayBasicExample />
+      <OverlayTriggerComp />
     </div>
   );
 };
---
+++ src/Component/MainBodySection/OverlayComponent/OverlayTriggerComp.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { OverlayTrigger, Button, Tooltip } from "react-bootstrap";
+export const OverlayTriggerComp = () => {
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Simple tooltip
+    </Tooltip>
+  );
+  return (
+    <div>
+      <OverlayTrigger
+        placement="right"
+        delay={{ show: 250, hide: 400 }}
+        overlay={renderTooltip}
+      >
+        <Button variant="success">Hover me to see</Button>
+      </OverlayTrigger>
+    </div>
+  );
+};
