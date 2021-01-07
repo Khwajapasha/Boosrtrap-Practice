3326f73af71ab8c0c03c5527a5ff6c3ad3418167
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-07 13:24:04 +0530

    Customizing overlay behavior

--- src/Component/MainBodySection/OverlayComponent/AllOverlay.jsx
+++ src/Component/MainBodySection/OverlayComponent/AllOverlay.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { OverlayBasicExample } from "./OverlayBasicExample";
 import { OverlayTriggerComp } from "./OverlayTriggerComp";
+import { OverlayCustomizingOverlayBehavior } from "./OverlayCustomizingOverlayBehavior";
 
 export const AllOverlay = () => {
   return (
     <div>
       <OverlayBasicExample />
       <OverlayTriggerComp />
+      <OverlayCustomizingOverlayBehavior />
     </div>
   );
 };
---
+++ src/Component/MainBodySection/OverlayComponent/OverlayCustomizingOverlayBehavior.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { OverlayTrigger, Image, Button, Tooltip } from "react-bootstrap";
+export const OverlayCustomizingOverlayBehavior = () => {
+  return (
+    <div>
+      <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip id="button-tooltip-2">Check out this avatar</Tooltip>}
+      >
+        {({ ref, ...triggerHandler }) => (
+          <Button
+            variant="light"
+            {...triggerHandler}
+            className="d-inline-flex align-items-center"
+          >
+            <Image
+              ref={ref}
+              roundedCircle
+              src="/Assets/BlueRose.jpg"
+              style={{ width: "2rem", height: "2rem" }}
+            />
+            <span className="ml-1">Hover to see</span>
+          </Button>
+        )}
+      </OverlayTrigger>
+    </div>
+  );
+};
