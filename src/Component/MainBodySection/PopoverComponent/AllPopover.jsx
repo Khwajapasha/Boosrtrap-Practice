62b3f4827c072650bbac3af41c8bf92878da1d4e
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-08 15:18:40 +0530

    Implemented onClick on all popover

--- src/Component/MainBodySection/PopoverComponent/AllPopover.jsx
+++ src/Component/MainBodySection/PopoverComponent/AllPopover.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { PopoverBasicExample } from "./PopoverBasicExample";
 import { PopoverPositions } from "./PopoverPositions";
 import { PopoverChangingContainer } from "./PopoverChangingContainer";
 import { PopoverUpdatingPositionDynamically } from "./PopoverUpdatingPositionDynamically";
 export const AllPopover = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <PopoverBasicExample />
-      <PopoverPositions />
-      <PopoverChangingContainer />
-      <PopoverUpdatingPositionDynamically />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <PopoverBasicExample />
+          <PopoverPositions />
+          <PopoverChangingContainer />
+          <PopoverUpdatingPositionDynamically />
+        </div>
+      )}
+      <div>
+        <Button onClick={() => setShow(!show)}>Load All Popovers</Button>
+      </div>
+    </React.Fragment>
   );
 };
