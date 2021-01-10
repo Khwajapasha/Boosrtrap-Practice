88abf886f3714c573b2ddf40708524a03ededc63
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-10 16:22:30 +0530

    Animated ProgressBar

---
+++ src/Component/MainBodySection/ProgressBarComponent/ProgressBarAnimated.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ProgressBar } from "react-bootstrap";
+export const ProgressBarAnimated = () => {
+  return (
+    <div>
+      <ProgressBar animated now={45} />
+    </div>
+  );
+};
