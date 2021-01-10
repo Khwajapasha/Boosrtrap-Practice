8114fa7a84605f71eb55974cb323dbbc1cb93dd2
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-10 16:33:50 +0530

    Implemented onClick event ProgressBar

--- src/Component/MainBodySection/ProgressBarComponent/AllProgressBar.jsx
+++ src/Component/MainBodySection/ProgressBarComponent/AllProgressBar.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProgressBarBasicExample } from "./ProgressBarBasicExample";
 import { ProgressBarContextualAlternative } from "./ProgressBarContextualAlternative";
 import { ProgressBarScreenReadOnly } from "./ProgressBarScreenReadOnly";
 import { ProgressBarWithLabel } from "./ProgressBarWithLabel";
 import { ProgressBarStrippedVariant } from "./ProgressBarStrippedVariant";
 import { ProgressBarAnimated } from "./ProgressBarAnimated";
+import { Button } from "react-bootstrap";
 
 export const AllProgressBar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <ProgressBarBasicExample />
-      <ProgressBarWithLabel />
-      <ProgressBarScreenReadOnly />
-      <ProgressBarContextualAlternative />
-      <ProgressBarStrippedVariant />
-      <ProgressBarAnimated />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <ProgressBarBasicExample />
+          <ProgressBarWithLabel />
+          <ProgressBarScreenReadOnly />
+          <ProgressBarContextualAlternative />
+          <ProgressBarStrippedVariant />
+          <ProgressBarAnimated />
+        </div>
+      )}
+      <div>
+        <Button onClick={() => setShow(!show)}>Load All ProgressBar</Button>
+      </div>
+    </React.Fragment>
   );
 };
