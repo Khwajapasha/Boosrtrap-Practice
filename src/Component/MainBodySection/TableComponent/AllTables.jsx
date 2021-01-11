2a929f119191243bc287c5ef01a1b809c32e8cf4
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-11 13:11:42 +0530

    Dark Theme of table

--- src/Component/MainBodySection/TableComponent/AllTables.jsx
+++ src/Component/MainBodySection/TableComponent/AllTables.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { TableBasicExample } from "./TableBasicExample";
 import { TableSmall } from "./TableSmall";
+import { TableDarkTheme } from "./TableDarkTheme";
 
 export const AllTables = () => {
   return (
     <div>
       <TableBasicExample />
       <TableSmall />
+      <TableDarkTheme />
     </div>
   );
 };
