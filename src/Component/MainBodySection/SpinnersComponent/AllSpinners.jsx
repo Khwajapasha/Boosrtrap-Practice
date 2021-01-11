981ade23470e796ffec6c2e900c5710ea9f3bfec
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-11 12:19:14 +0530

    Implemented onClick event on All Spinners

--- src/Component/MainBodySection/SpinnersComponent/AllSpinners.jsx
+++ src/Component/MainBodySection/SpinnersComponent/AllSpinners.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { SnippersInButtons } from "./SnippersInButtons";
 import { SpinnerAnimation } from "./SpinnerAnimation";
 import { SpinnerBasicExample } from "./SpinnerBasicExample";
 import { SpinnerSizing } from "./SpinnerSizing";
 import { SpinnerVariants } from "./SpinnerVariants";
 export const AllSpinners = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <SpinnerBasicExample />
-      <SpinnerAnimation />
-      <SpinnerVariants />
-      <SpinnerSizing />
-      <SnippersInButtons />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <SpinnerBasicExample />
+          <SpinnerAnimation />
+          <SpinnerVariants />
+          <SpinnerSizing />
+          <SnippersInButtons />
+        </div>
+      )}
+      <div>
+        <Button onClick={() => setShow(!show)}>Load All Spinners</Button>
+      </div>
+    </React.Fragment>
   );
 };
