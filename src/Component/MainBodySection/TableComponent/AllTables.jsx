fecef59d2ac74bd972c9a7072065dfda3eed036c
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-12 15:46:48 +0530

    Working on all tables component

--- src/Component/MainBodySection/TableComponent/AllTables.jsx
+++ src/Component/MainBodySection/TableComponent/AllTables.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TableBasicExample } from "./TableBasicExample";
 import { TableSmall } from "./TableSmall";
 import { TableDarkTheme } from "./TableDarkTheme";
@@ -6,6 +6,7 @@ import { TableAlwaysResponsive } from "./TableAlwaysResponsive";
 import { TableSpecificBreakPoint } from "./TableSpecificBreakPoint";
 
 export const AllTables = () => {
+  const [show, setShow] = useState(false);
   return (
     <div>
       <TableBasicExample />
