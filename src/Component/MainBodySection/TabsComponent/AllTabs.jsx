d2f592585721384cbcbda4b4b337cb33f0210b20
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-13 11:09:37 +0530

    Imported TabWithoutAnimation to All Tab

--- src/Component/MainBodySection/TabsComponent/AllTabs.jsx
+++ src/Component/MainBodySection/TabsComponent/AllTabs.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { TabBasicExample } from "./TabBasicExample";
 import { TabControlled } from "./TabControlled";
+import { TabWithoutAnimation } from "./TabWithoutAnimation";
 
 export const AllTabs = () => {
   return (
     <div>
       <TabBasicExample />
       <TabControlled />
+      <TabWithoutAnimation />
     </div>
   );
 };
