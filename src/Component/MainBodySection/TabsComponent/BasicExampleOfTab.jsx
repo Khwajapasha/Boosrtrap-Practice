3d0ec7268c58081d46a2cd9a145bb89e3d6fbbb0
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-13 10:34:46 +0530

    Set the position of tab

--- src/Component/MainBodySection/TabsComponent/BasicExampleOfTab.jsx
+++ src/Component/MainBodySection/TabsComponent/BasicExampleOfTab.jsx
@@ -6,20 +6,26 @@ import { CardImageOverlay } from "../CardsComponent/CardImageOverlay";
 export const BasicExampleOfTab = () => {
   return (
     <div>
-      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-        <Tab eventKey="home" title="Home">
-          <CardBasicExample />
-        </Tab>
-        <Tab eventKey="profile" title="Profile">
-          <CardImageOverlay />
-        </Tab>
-        <Tab eventKey="contact" title="Contact">
-          <CardHeaderAndFooter />
-        </Tab>
-        <Tab eventKey="contact" title="Contact" disabled>
-          <CardHeaderAndFooter />
-        </Tab>
-      </Tabs>
+      <br />
+      <h4 style={{ float: "left" }}>=== Basic Example Of Tab ===</h4>
+      <br />
+      <br />
+      <div>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+          <Tab eventKey="home" title="Home">
+            <CardBasicExample />
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+            <CardImageOverlay />
+          </Tab>
+          <Tab eventKey="contact" title="Contact">
+            <CardHeaderAndFooter />
+          </Tab>
+          <Tab eventKey="dissable" title="Contact" disabled>
+            <CardHeaderAndFooter />
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 };
