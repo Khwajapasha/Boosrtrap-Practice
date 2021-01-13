fade8f30b30f2ae9ad6cf4d204e2ccfe02d6c84c
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-13 11:17:46 +0530

    Working on useState

--- src/Component/MainBodySection/TabsComponent/AllTabs.jsx
+++ src/Component/MainBodySection/TabsComponent/AllTabs.jsx
@@ -1,11 +1,12 @@
 import { Button } from "bootstrap";
-import React from "react";
+import React, { useState } from "react";
 import { TabBasicExample } from "./TabBasicExample";
 import { TabControlled } from "./TabControlled";
 import { TabCustomLayout } from "./TabCustomLayout";
 import { TabWithoutAnimation } from "./TabWithoutAnimation";
 
 export const AllTabs = () => {
+  const [show, setShow] = useState(false);
   return (
     <React.Fragment>
       <div>
