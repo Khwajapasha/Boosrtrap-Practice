aee718d257f82577e3f04aa49ee8caf7b00814c4
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-14 15:08:50 +0530

    Update toast stacking

--- src/Component/MainBodySection/ToastComponent/AllToast.jsx
+++ src/Component/MainBodySection/ToastComponent/AllToast.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { ToastBaiscExample } from "./ToastBaiscExample";
 import { ToastDismissable } from "./ToastDismissable";
-
+import { ToastStacking } from "./ToastStacking";
 export const AllToast = () => {
   return (
     <div>
       <ToastBaiscExample />
       <ToastDismissable />
+      <ToastStacking />
     </div>
   );
 };
