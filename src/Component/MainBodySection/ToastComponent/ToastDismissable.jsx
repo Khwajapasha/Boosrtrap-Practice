cce619d1d585c9f41050d9c65ab5d5f9ebb1ca61
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-14 11:32:10 +0530

    working on dismissable toast

---
+++ src/Component/MainBodySection/ToastComponent/ToastDismissable.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export const ToastDismissable = () => {
+  const [showA, setShowA] = useState(true);
+  const [showB, setShowB] = useState(true);
+  return <div></div>;
+};
