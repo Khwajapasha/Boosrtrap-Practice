db9ff24230845fb00b95bbf31e8332ff71f6aa5a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-14 15:31:51 +0530

    Toast AutoHide

---
+++ src/Component/MainBodySection/ToastComponent/ToastAutoHide.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Row, Col, Toast, Button } from "react-bootstrap";
+export const ToastAutoHide = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <Row>
+        <Col xs={6}>
+          <Toast
+            onClose={() => setShow(false)}
+            show={show}
+            delay={3000}
+            autohide
+          >
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded mr-2"
+                alt=""
+              />
+              <strong className="mr-auto">Bootstrap</strong>
+              <small>11 mins ago</small>
+            </Toast.Header>
+            <Toast.Body>
+              Woohoo, you're reading this text in a Toast!
+            </Toast.Body>
+          </Toast>
+        </Col>
+        <Col xs={6}>
+          <Button onClick={() => setShow(true)}>Show Toast</Button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
