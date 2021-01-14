527f686f74d26964b6c5cfb2cf4b38b8425ce754
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-14 15:25:45 +0530

    update toast placement

--- src/Component/MainBodySection/ToastComponent/ToastAlignment.jsx
+++ src/Component/MainBodySection/ToastComponent/ToastAlignment.jsx
@@ -32,6 +32,54 @@ export const ToastAlignment = () => {
         </Toast.Header>
         <Toast.Body>See? Just like this.</Toast.Body>
       </Toast>
+      <br />
+      <br />
+      <br />
+      <p>
+        For systems that generate more notifications, consider using a wrapping
+        element so they can easily stack.
+      </p>
+      <div
+        aria-live="polite"
+        aria-atomic="true"
+        style={{
+          position: "relative",
+          minHeight: "200px",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+        >
+          <Toast>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded mr-2"
+                alt=""
+              />
+              <strong className="mr-auto">Bootstrap</strong>
+              <small>just now</small>
+            </Toast.Header>
+            <Toast.Body>See? Just like this.</Toast.Body>
+          </Toast>
+          <Toast>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded mr-2"
+                alt=""
+              />
+              <strong className="mr-auto">Bootstrap</strong>
+              <small>2 seconds ago</small>
+            </Toast.Header>
+            <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
+          </Toast>
+        </div>
+      </div>
     </div>
   );
 };
