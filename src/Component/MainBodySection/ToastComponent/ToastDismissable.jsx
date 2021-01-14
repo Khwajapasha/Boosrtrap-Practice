cca6a945d88060587cd1eb0d22020130a1677831
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-14 11:38:22 +0530

    Dismissable toast

--- src/Component/MainBodySection/ToastComponent/ToastDismissable.jsx
+++ src/Component/MainBodySection/ToastComponent/ToastDismissable.jsx
@@ -1,7 +1,57 @@
 import React, { useState } from "react";
-
+import { Toast, Row, Col, Button } from "react-bootstrap";
 export const ToastDismissable = () => {
   const [showA, setShowA] = useState(true);
   const [showB, setShowB] = useState(true);
-  return <div></div>;
+
+  const toggleShowA = () => setShowA(!showA);
+  const toggleShowB = () => setShowB(!showB);
+  return (
+    <div>
+      <Row>
+        <Col xs={6}>
+          <Toast show={showA} onClose={toggleShowA}>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded mr-2 mb-2"
+                alt=""
+              />
+              <strong className="mr-auto">Bootstrap</strong>
+              <small>11 mins ago</small>
+            </Toast.Header>
+            <Toast.Body>
+              Woohoo, you're reading this text in a Toast!
+            </Toast.Body>
+          </Toast>
+        </Col>
+        <Col xs={6}>
+          <Button onClick={toggleShowA}>
+            Toggle Toast <strong>with</strong> Animation
+          </Button>
+        </Col>
+        <Col xs={6} className="my-1">
+          <Toast onClose={toggleShowB} show={showB} animation={false}>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded mr-2 mt-2"
+                alt=""
+              />
+              <strong className="mr-auto">Bootstrap</strong>
+              <small>11 mins ago</small>
+            </Toast.Header>
+            <Toast.Body>
+              Woohoo, you're reading this text in a Toast!
+            </Toast.Body>
+          </Toast>
+        </Col>
+        <Col xs={6}>
+          <Button onClick={toggleShowB}>
+            Toggle Toast <strong>without</strong> Animation
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
 };
