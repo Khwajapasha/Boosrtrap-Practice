2077873854fc59dadd602ebc42d4691a0b84a415
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-15 14:44:00 +0530

    Onclick event on all toast

--- src/Component/MainBodySection/ToastComponent/AllToast.jsx
+++ src/Component/MainBodySection/ToastComponent/AllToast.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { ToastAlignment } from "./ToastAlignment";
 import { ToastAutoHide } from "./ToastAutoHide";
 import { ToastBaiscExample } from "./ToastBaiscExample";
 import { ToastDismissable } from "./ToastDismissable";
 import { ToastStacking } from "./ToastStacking";
 export const AllToast = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <ToastBaiscExample />
-      <ToastDismissable />
-      <ToastStacking />
-      <ToastAlignment />
-      <ToastAutoHide />
-    </div>
+    <React.Fragment>
+      {show && (
+        <div>
+          <ToastBaiscExample />
+          <ToastDismissable />
+          <ToastStacking />
+          <ToastAlignment />
+          <ToastAutoHide />
+        </div>
+      )}
+      <div>
+        <Button onClick={() => setShow(!show)}>Load all Toast</Button>
+      </div>
+    </React.Fragment>
   );
 };
