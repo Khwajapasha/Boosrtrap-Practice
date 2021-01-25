a1a29f0281bcc5afbef4e271dcf1e1d601b5ca5c
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-25 13:24:58 +0530

    Implemented media as a list

--- src/Component/MainBodySection/MediaComponent/MediaASList.jsx
+++ src/Component/MainBodySection/MediaComponent/MediaASList.jsx
@@ -3,6 +3,9 @@ import { Media } from "react-bootstrap";
 export const MediaASList = () => {
   return (
     <div className="mt-4">
+      <br />
+      <h5>===Media as a list===</h5>
+      <br />
       <ul className="list-unstyled">
         <Media as="li" style={{ backgroundColor: "pink" }}>
           <img
@@ -32,6 +35,24 @@ export const MediaASList = () => {
           />
           <Media.Body>
             <h5>List-based media object 2</h5>
+            <p>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+              scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+              in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </p>
+          </Media.Body>
+        </Media>
+        <Media as="li" style={{ backgroundColor: "pink" }} className="mt-2">
+          <img
+            width={64}
+            height={64}
+            className="mr-3"
+            src="Assets/Rose.jpg"
+            alt="Generic placeholder"
+          />
+          <Media.Body>
+            <h5>List-based media object</h5>
             <p>
               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
               scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
