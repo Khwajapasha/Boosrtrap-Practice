e11e03b77c790fcf8aa95d08f334d3ecdecd455a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-25 13:09:21 +0530

    Ordered Media

--- src/Component/MainBodySection/MediaComponent/AllMedia.jsx
+++ src/Component/MainBodySection/MediaComponent/AllMedia.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { MediaAlignment } from "./MediaAlignment";
 import { MediaBasicExample } from "./MediaBasicExample";
 import { MediaNesting } from "./MediaNesting";
+import { MediaOrdered } from "./MediaOrdered";
 export const AllMedia = () => {
   return (
     <div>
       <MediaBasicExample />
       <MediaNesting />
       <MediaAlignment />
+      <MediaOrdered />
     </div>
   );
 };
