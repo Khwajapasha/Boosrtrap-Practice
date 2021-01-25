b404a5b3c7257c1974e60b42ca3408dc317d5ba2
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-25 13:27:40 +0530

    Working on all media

--- src/Component/MainBodySection/MediaComponent/AllMedia.jsx
+++ src/Component/MainBodySection/MediaComponent/AllMedia.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import { MediaAlignment } from "./MediaAlignment";
 import { MediaASList } from "./MediaASList";
 import { MediaBasicExample } from "./MediaBasicExample";
@@ -6,12 +7,17 @@ import { MediaNesting } from "./MediaNesting";
 import { MediaOrdered } from "./MediaOrdered";
 export const AllMedia = () => {
   return (
-    <div>
-      <MediaBasicExample />
-      <MediaNesting />
-      <MediaAlignment />
-      <MediaOrdered />
-      <MediaASList />
-    </div>
+    <React.Fragment>
+      <div>
+        <MediaBasicExample />
+        <MediaNesting />
+        <MediaAlignment />
+        <MediaOrdered />
+        <MediaASList />
+      </div>
+      <div>
+        <Button>Load All Media</Button>
+      </div>
+    </React.Fragment>
   );
 };
