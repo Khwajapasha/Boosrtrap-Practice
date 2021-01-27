a51fb11f426d9f9cf5e5bbc25ac3a4e4e68fd6c3
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-27 12:55:18 +0530

    removed unnessesory code & component

--- src/Component/Header Section/HeaderSection.jsx
+++ src/Component/Header Section/HeaderSection.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { AllJumbotron } from "./JumbotronComponent/AllJumbotron";
+import { AddMultipleDivOnClick } from "./AddMultipleDivOnclik/AddMultipleDivOnClick";
+
 export const HeaderSection = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <AllJumbotron />
-    </div>
+    <React.Fragment>
+      <div>
+        <AllJumbotron />
+        <AddMultipleDivOnClick />
+      </div>
+      <div>
+        <Button>Load All Header Component</Button>
+      </div>
+    </React.Fragment>
   );
 };
--- src/Component/HomePage/Home.jsx
+++ src/Component/HomePage/Home.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { HeaderSection } from "../Header Section/HeaderSection";
 import { MainBodySection } from "../MainBodySection/MainBodySection";
 import { LeftNavigationSection } from "../LeftNavigationSection/LeftNavigationSection";
-import NavigationBar from "./NavigationBar";
-import { AddMultipleDivOnClick } from "../AddMultipleDivOnClick";
 export const Home = () => {
   return (
     <div>
-      <AddMultipleDivOnClick />
-      <NavigationBar />
       <HeaderSection />
       <LeftNavigationSection />
       <MainBodySection />
