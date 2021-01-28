7fa506545fd7adb0b3ee6844bd05f3dd0bf3211d
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-28 12:38:13 +0530

    Removed unnessesory code

--- src/Component/LeftNavigationSection/LeftNavigationSection.jsx
+++ src/Component/LeftNavigationSection/LeftNavigationSection.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { LeftNavigationItems } from './LeftNavigationItems';
-
+import React from "react";
+import { Button } from "react-bootstrap";
 export const LeftNavigationSection = () => {
-    return (
-        <div>
-            <LeftNavigationItems/>
-        </div>
-    );
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Implemented All Material UI Component</h1>
+      </div>
+      <div>
+        <Button>Load All Material UI Component</Button>
+      </div>
+    </React.Fragment>
+  );
 };
-
- 
