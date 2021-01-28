d19136c8905c7206b80fc93f443164e415028c1c
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-28 13:18:29 +0530

    Imple grid spacing

--- src/Component/LeftNavigationSection/Layout/AllLayout.jsx
+++ src/Component/LeftNavigationSection/Layout/AllLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ContainerBasicExample } from "./ContainerComponent/ContainerBasicExample";
 import { BoxBasicExample } from "./BoxComponent/BoxBasicExample";
+import AllGrid from "./GridComponent/AllGrid";
 export const AllLayout = () => {
   return (
     <div>
       <BoxBasicExample />
       <ContainerBasicExample />
+      <AllGrid />
     </div>
   );
 };
---
+++ src/Component/LeftNavigationSection/Layout/GridComponent/AllGrid.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GridSpacing } from "./GridSpacing";
+export const AllGrid = () => {
+  return (
+    <div>
+      <GridSpacing />
+    </div>
+  );
+};
+
+export default AllGrid;
