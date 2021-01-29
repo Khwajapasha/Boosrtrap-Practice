52924952bdaf8df1a82140b2a65446dfab12c3dc
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-29 14:51:12 +0530

    Button Outline

--- src/Component/LeftNavigationSection/InputComponent/ButtonComponent/AllButton.jsx
+++ src/Component/LeftNavigationSection/InputComponent/ButtonComponent/AllButton.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ButtonBasicExample } from "./ButtonBasicExample";
+import { ButtonOutline } from "./ButtonOutline";
 import { TextButton } from "./TextButton";
 export const AllButton = () => {
   return (
     <div>
       <ButtonBasicExample />
       <TextButton />
+      <ButtonOutline />
     </div>
   );
 };
