25926f5129e480ac383af38ff11f80f34f5e37f3
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-29 12:44:27 +0530

    Impl basic example of grid

--- src/Component/LeftNavigationSection/Layout/GridComponent/AllGrid.jsx
+++ src/Component/LeftNavigationSection/Layout/GridComponent/AllGrid.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { GridBasicExample } from "./GridBasicExample";
 import { GridSpacing } from "./GridSpacing";
 export const AllGrid = () => {
   return (
     <div>
       <GridSpacing />
+      <GridBasicExample />
     </div>
   );
 };
--- src/Component/LeftNavigationSection/Layout/GridComponent/GridBasicExample.jsx
+++ src/Component/LeftNavigationSection/Layout/GridComponent/GridBasicExample.jsx
@@ -16,9 +16,29 @@ const useStyles = makeStyles((theme) => ({
 export const GridBasicExample = () => {
   const classes = useStyles();
   return (
-    <div>
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>xs=12</Paper>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
       </Grid>
     </div>
   );
