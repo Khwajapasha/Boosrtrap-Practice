4857102c80c99d9ac83c3d1b5cbeb5219dc86318
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-29 12:46:41 +0530

    Working on grid specific break point

---
+++ src/Component/LeftNavigationSection/Layout/GridComponent/GridSpecificBreakPoint.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export const GridSpecificBreakPoint = () => {
+  return <div></div>;
+};
