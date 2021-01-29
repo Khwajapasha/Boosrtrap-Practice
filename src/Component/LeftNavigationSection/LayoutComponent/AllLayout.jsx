9dc1f08940ca2a64c2c34b1e3e56c8a859149e92
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-29 14:40:04 +0530

    impl onClick on all layout

--- src/Component/LeftNavigationSection/LayoutComponent/AllLayout.jsx
+++ src/Component/LeftNavigationSection/LayoutComponent/AllLayout.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { ContainerBasicExample } from "./ContainerComponent/ContainerBasicExample";
 import { BoxBasicExample } from "./BoxComponent/BoxBasicExample";
 import AllGrid from "./GridComponent/AllGrid";
 export const AllLayout = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <BoxBasicExample />
-      <ContainerBasicExample />
-      <AllGrid />
-    </div>
+    <React.Fragment>
+      <div className="mt-2">
+        <Button onClick={() => setShow(!show)}>Load Layout</Button>
+      </div>
+      {show && (
+        <div>
+          <BoxBasicExample />
+          <ContainerBasicExample />
+          <AllGrid />
+        </div>
+      )}
+    </React.Fragment>
   );
 };
